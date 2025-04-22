efc9bf8b40b036b0dc688b2e0c0fb4661a842af7
ABDELLAH MOUTAWAKKIL <abdo.moutawakkil2002@gmail.com>
2025-04-22 17:07:01 +0100

    first commit

---
+++ src/test/java/com/abdellah/collegepaymentmanagement/CollegePaymentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdellah.collegepaymentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegePaymentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
